e4e3f32f544781c232461a52655c9c1894f8b0a4
Juanfe <gjuanfelipe45@gmail.com>
2025-09-09 00:24:46 -0500

    primer commit

---
+++ src/main/java/com/eam/LevelUpCorp/LevelUpCorpApplication.java
@@ -0,0 +1,13 @@
+package com.eam.LevelUpCorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpCorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpCorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eam/LevelUpCorp/LevelUpCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.LevelUpCorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpCorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
