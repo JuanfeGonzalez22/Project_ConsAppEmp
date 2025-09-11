d67721ede0b7243dcfa9f07e5d20a49d02a79898
Sammy <cordoba.samuel.5631@eam.edu.co>
2025-09-11 11:31:48 -0500

    Added Controllers

---
+++ src/main/controller/CertificateController.java
@@ -0,0 +1,9 @@
+package com.empresa.training.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/certificates")
+public class CertificateController {
+
+}
---
+++ src/main/controller/EvaluationController.java
@@ -0,0 +1,9 @@
+package com.empresa.training.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/evaluations")
+public class EvaluationController {
+
+}
