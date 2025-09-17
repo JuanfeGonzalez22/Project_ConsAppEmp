37506f201f41bcc936405fe2e70d2f3d1baa8005
Juanfe <gjuanfelipe45@gmail.com>
2025-09-17 12:39:22 -0500

    doc(dto): Documentado ModuleDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/ModuleDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/ModuleDTO.java
@@ -1,5 +1,15 @@
 package com.eam.LevelUpCorp.businessLayer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO que representa un módulo dentro de un curso")
 public class ModuleDTO {
 
 
