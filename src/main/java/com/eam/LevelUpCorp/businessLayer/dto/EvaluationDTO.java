a93a3030252d11f2d04398b1e431f9d447f26f39
Juanfe <gjuanfelipe45@gmail.com>
2025-09-17 12:23:58 -0500

    doc(dto): Documentado EvaluationDTO con @schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/EvaluationDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/EvaluationDTO.java
@@ -1,5 +1,9 @@
 package com.eam.LevelUpCorp.businessLayer.dto;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Este es un objeto de datos que muestra una evaluacion con moduloId, titulo, tipo y un puntaje Maximo para asignar.")
 public class EvaluationDTO {
 
 
