357f1141127e2664a9a0c0d6f7e2c71367362a60
Juanfe <gjuanfelipe45@gmail.com>
2025-09-16 22:50:31 -0500

    docs(dto): documentado CertificateDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/CertificateDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/CertificateDTO.java
@@ -2,6 +2,11 @@ package com.eam.LevelUpCorp.businessLayer.dto;
 
 import java.time.LocalDate;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Este es un objeto de datos que muestra un certificado con usuario, curso, fecha de emisión y un hash para verificar.")
 public class CertificateDTO {
 
     private int usuarioId;
