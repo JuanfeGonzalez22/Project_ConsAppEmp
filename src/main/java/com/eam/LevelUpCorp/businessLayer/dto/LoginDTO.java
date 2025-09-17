36831e6a321dc114939114e5a6af8be16bcb7221
Juanfe <gjuanfelipe45@gmail.com>
2025-09-17 12:36:58 -0500

    doc(dto): Documentado LoginDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/LoginDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/LoginDTO.java
@@ -1,8 +1,12 @@
 package com.eam.LevelUpCorp.businessLayer.dto;
-
-
-
-
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Este DTO se usa para hacer un login en la plataforma")
 public class LoginDTO {
 
     private String email;
