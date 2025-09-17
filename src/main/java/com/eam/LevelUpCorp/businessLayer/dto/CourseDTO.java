5d3e452f3a17e7f6d1a1e07c35a4a9ffb64c8964
Juanfe <gjuanfelipe45@gmail.com>
2025-09-16 23:04:03 -0500

    doc(dto): documentado CourseDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/CourseDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/CourseDTO.java
@@ -2,6 +2,10 @@ package com.eam.LevelUpCorp.businessLayer.dto;
 
 import java.time.LocalTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Este es un objeto de datos que muestra un curso con descripcion, titulo, duracion Estimada y un nivel para asignar.")
 public class CourseDTO {
 
     private String descripcion;
