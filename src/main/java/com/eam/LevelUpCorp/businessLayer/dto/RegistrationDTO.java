14eb8c8dd60f9563d193bd2cdb5606d1d67e99d0
Juanfe <gjuanfelipe45@gmail.com>
2025-09-17 12:41:09 -0500

    doc(dto): Documentado RegistrationDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/RegistrationDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/RegistrationDTO.java
@@ -1,4 +1,13 @@
 package com.eam.LevelUpCorp.businessLayer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO usado para registrar un nuevo usuario en la plataforma")
 public class RegistrationDTO {
 }
