14e9b7aa60e87d02f8190456c20be6aada9ec161
Juanfe <gjuanfelipe45@gmail.com>
2025-09-17 12:43:46 -0500

    doc(dto): Documentado UserDTO con @Schema

--- src/main/java/com/eam/LevelUpCorp/businessLayer/dto/UserDTO.java
+++ src/main/java/com/eam/LevelUpCorp/businessLayer/dto/UserDTO.java
@@ -1,5 +1,14 @@
 package com.eam.LevelUpCorp.businessLayer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO que representa la información básica de un usuario en la plataforma")
 public class UserDTO {
 
     private String nombre;
